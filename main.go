61376cfc486305b6724ec169abf9fd4976de4362
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-03-14 16:23:14 +0100

    creating a type deck

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	//declare a variable
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// fmt.Println(card)
+	cards := deck{newCard(), "Ace of Spades"}
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	fmt.Println(cards)
 }
 
-func newCard() string{
+func newCard() string {
 	return "Five of Diamonds"
 }
- 
