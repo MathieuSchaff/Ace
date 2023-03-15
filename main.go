7996eef7a0aa59900ccc8b07572b3debe3af879b
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-03-15 22:00:04 +0100

    testing deck

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
-	//declare a variable
-	// card := newCard()
-	// fmt.Println(card)
-	cards := deck{newCard(), "Ace of Spades"}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	fmt.Println(cards)
-}
 
-func newCard() string {
-	return "Five of Diamonds"
+	// cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	// hand, remainingCards := deal(cards, 10)
+	// hand.print()
+	// remainingCards.print()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
 }
